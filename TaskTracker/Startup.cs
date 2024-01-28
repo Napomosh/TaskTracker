7048cfc40bd6b9fd7a7aca97e087ff2f6abfb9ae
Napomosh <sashulik999@gmail.com>
2024-01-28 23:54:53 +0300

    Init commit

---
+++ TaskTracker/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskTracker;
+
+public class Startup{
+    public static void ConfigureServices(IServiceCollection service){
+        service.AddRazorPages();
+    }
+
+    public static void Configure(IApplicationBuilder app, IHostEnvironment env){
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+        else
+            app.UseExceptionHandler("/Error");
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseEndpoints(endpoints => {
+            endpoints.MapRazorPages();
+        });
+    }
+}
